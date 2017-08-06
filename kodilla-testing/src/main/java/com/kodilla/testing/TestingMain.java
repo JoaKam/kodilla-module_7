1ad4715463fefa4792371eb75ca8e472c70a6b8a
JoaKam <joannakamienik@wp.pl>
2017-08-06 21:08:50 +0200

    exercise 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,28 @@
 package com.kodilla.testing;
 
-public class TestingMain {
-    public static void main(String[] args){
+class TestingMain {
+    public static void main(String[] args) {
         System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
 
+    /* tests  */
+
+        int a = 5;
+        int b = 3;
+        int addResult = a + b;
+        int subtractResult = a - b;
+
+        Calculator newCalculator = new Calculator();
+
+        if (addResult == newCalculator.add2Numbers(a, b)) {
+            System.out.println("Add test OK");
+        } else {
+            System.out.println("Error in adding!");
+        }
+
+        if (subtractResult == newCalculator.subtract2Numbers(a, b)) {
+            System.out.println("Subtract test OK");
+        } else {
+            System.out.println("Error in subtraction!");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,18 @@
+package com.kodilla.testing.calculator;
+
+class Calculator {
+
+    public Calculator() {
+    }
+
+    public int add2Numbers(int firstNumber, int secondNumber) {
+        int sum = firstNumber + secondNumber;
+        return sum;
+    }
+
+    public int subtract2Numbers(int firstNumber, int secondNumber) {
+        int difference = firstNumber - secondNumber;
+        return difference;
+    }
+
+}
