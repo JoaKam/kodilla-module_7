ebf6ff564e174fbe2872b3d78ae3e3cff3c1908d
JoaKam <joannakamienik@wp.pl>
2017-08-07 21:27:46 +0200

    Exercise 6.3 initial

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -0,0 +1,12 @@
+package com.kodilla.testing.collection;
+
+import java.util.ArrayList;
+
+class OddNumbersExterminator {
+
+    public ArrayList<Integer> exterminate(ArrayList<Integer> numbers) {
+        ArrayList<Integer> evenNumbersList;
+/* kod pomijania liczb nieparzystych */
+        return evenNumbersList;
+    }
+}
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -0,0 +1,20 @@
+
+package com.kodilla.testing.collection;
+import com.kodilla.testing.collection.OddNumbersExterminator;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CollectionTestSuite {
+
+    /*użyć before i after*/
+
+    @Test
+    public void testOddNumbersExterminatorEmptyList(){
+        /*czy klasa zachowuje się poprawnie przy pustej liście */
+    }
+
+    public void testOddNumbersExterminatorNormalList(){
+         /*czy klasa ok gdy lista zawiera parzyste i nieparzyste*/
+    }
+
+}
